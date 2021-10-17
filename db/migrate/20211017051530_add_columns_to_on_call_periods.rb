8d8509b38586e6708973226500fd3d0ba9112fc4
donghee <kimu.donhi@gmail.com>
2021-10-18 03:39:16 +0900

    Fix typo

--- db/migrate/20211017051530_add_columns_to_on_call_periods.rb
+++ db/migrate/20211017051530_add_columns_to_on_call_periods.rb
@@ -1,6 +1,6 @@
 class AddColumnsToOnCallPeriods < ActiveRecord::Migration[6.1]
   def change
-    add_column :on_call_periods, :start_date, :datetime,
+    add_column :on_call_periods, :start_date, :datetime
     add_column :on_call_periods, :end_date, :datetime
   end
 end
